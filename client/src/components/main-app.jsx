3c4186554a0961bb273872d696a0e021989baadf
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-03-02 14:20:58 +0100

    remove app component and add main-app

---
+++ client/src/components/main-app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is MainApp</h3>
+                <p>Siavash should implement this home-page</p>
+            </div>
+        );
+    }
+}
+
+export default MainApp;
